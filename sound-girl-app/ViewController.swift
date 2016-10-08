a172028b90fb20bcd58081ba4e75ca33140a85db
toco <tokoken1001@gmail.com>
2016-10-09 00:52:01 +0700

    Initial Commit

---
+++ sound-girl-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sound-girl-app
+//
+//  Created by toco on 09/10/16.
+//  Copyright © 2016 chihiro&toco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
